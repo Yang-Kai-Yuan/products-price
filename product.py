4bbb7f44785a01f326036d6f536338dc0498ddaf
yang-kai-yuan <haha200ha@gmail.com>
2020-10-05 13:53:19 +0800

    add-write2

--- product.py
+++ product.py
@@ -1,4 +1,15 @@
+# 讀取檔案
 products = []
+
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name,price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+# 使用者操作
 while True:
 	name = input('請輸入商品名稱:(不再輸入請按q)')
 	if name == 'q':
@@ -7,8 +18,11 @@ while True:
 	products.append([name, price])
 print(products)
 
+# 清單呈現
 for p in products:
-    print(p[0], p[1], '元')
+    print(p[0], p[1])
+
+# 寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品,價格'+ '\n')
 	for p in products:
